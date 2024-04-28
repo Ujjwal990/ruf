03f30b819158d3bf5159b28e3531490aef7e385e
ujjwal <ujjwalkumar080302@gmail.com>
2024-04-28 19:32:01 +0530

    first commit in master branch

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-# python is a very good language
-this is the new line
-
-// this must be checked
-entered
-
-def fn():
-    print(" i am right here")
-
-
-fn4 added
+urlslist = ["first url", "second_url"]
