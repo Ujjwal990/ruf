2f6954d75a903fba79e791813fadf1f02188fa0d
ujjwal <ujjwalkumar080302@gmail.com>
2024-04-28 19:30:04 +0530

    new function added

--- index.py
+++ index.py
@@ -1,11 +1,24 @@
-# python is a very good language
-this is the new line
+# Python program to print all
+# prime number in an interval
+def prime(x, y):
+    prime_list = []
+    for i in range(x, y):
+        if i == 0 or i == 1:
+            continue
+        else:
+            for j in range(2, int(i/2)+1):
+                if i % j == 0:
+                    break
+            else:
+                prime_list.append(i)
+    return prime_list
 
-// this must be checked
-entered
 
-def fn():
-    print(" i am right here")
-
-
-fn4 added
+# Driver program
+starting_range = 2
+ending_range = 7
+lst = prime(starting_range, ending_range)
+if len(lst) == 0:
+    print("There are no prime numbers in this range")
+else:
+    print("The prime numbers in this range are: ", lst)
